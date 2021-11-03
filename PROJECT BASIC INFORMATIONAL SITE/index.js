a82da80ca951f4631730eeaa2500665600c5549a
Batuhan Erdeniz <44765515+modifci@users.noreply.github.com>
2021-11-03 16:21:31 +0300

    Add files via upload

---
+++ PROJECT BASIC INFORMATIONAL SITE/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/about.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/about.html'))
+})
+
+app.get('/contact.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/contact.html'))
+})
+
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '404.html'))
+})
+app.listen(port, () => {
+  console.log('Running app...')
+})
